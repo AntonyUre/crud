485579652bc5c1fe04aed0b3a41647cbf5ffa1e2
antonyur <ureta.art@hotmail.com>
2020-08-25 23:44:12 -0500

    se culmino el ui

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const close = document.querySelector('.close-modal');
+const open = document.querySelector('.open-modal');
+const containerModal = document.querySelector('.modal-container');
+
+window.onclick =function(e){
+    
+    if(e.target == containerModal){
+        containerModal.style.display='none';
+    }
+}
+
+close.addEventListener('click',function(){
+    containerModal.style.display='none';
+    
+})
+open.addEventListener('click', function(){
+    containerModal.style.display='block';
+    
+
+})
