99d236f4d22c3a35e9ce2f8160e8a0746457dd27
antonyur <ureta.art@hotmail.com>
2020-08-27 01:08:12 -0500

    se implemento correctamente el modal

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
-const close = document.querySelector('.close-modal');
-const open = document.querySelector('.open-modal');
-const containerModal = document.querySelector('.modal-container');
+const close = document.querySelector(".close-modal");
+const open = document.querySelector(".open-modal");
+const containerModal = document.querySelector(".windows-modal");
 
-window.onclick =function(e){
-    
-    if(e.target == containerModal){
-        containerModal.style.display='none';
-    }
-}
+window.onclick = function (e) {
+  if (e.target == containerModal) {
+    containerModal.style.display = "none";
+  }
+};
 
-close.addEventListener('click',function(){
-    containerModal.style.display='none';
-    
-})
-open.addEventListener('click', function(){
-    containerModal.style.display='block';
-    
+close.addEventListener("click", function () {
+  containerModal.style.opacity = "0";
+  containerModal.style.visibility = "hidden";
+});
 
-})
+open.addEventListener("click", function () {
+  containerModal.style.opacity = "1";
+  containerModal.style.visibility = "visible";
+});
