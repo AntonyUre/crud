740957b24f5eef3b9e16b6539cb4dfa504eecaab
Mirian Quispe <mquispes@proveedor.intercorp.com.pe>
2020-08-27 19:21:47 -0500

    add module

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
+import Modal from './modal.js';
+ const button = document.querySelector('.open-modal');
+ const modal = new Modal(document.querySelector('.windows-modal'));
+ console.log(button);
+// console.log(modal);
+ 
+ button.onclick = function(){
+   modal.open();
+ }
 
+
+
+
+
+
+
+
+
+
+/*
 
 const close = document.querySelector(".close-modal");
 const open = document.querySelector(".open-modal");
@@ -18,4 +37,4 @@ close.addEventListener("click", function () {
 open.addEventListener("click", function () {
   containerModal.style.opacity = "1";
   containerModal.style.visibility = "visible";
-});
+});*/
---
+++ js/modal.js
@@ -0,0 +1,19 @@
+function Modal(element) {
+  this.element = element;
+  this.close = this.element.querySelector(".close-modal");
+  this.accept = this.element.querySelector(".js_accept");
+  this.cancel = this.element.querySelector(".js_cancel"); 
+}
+
+Modal.prototype.open = function(){
+  this.element.style.opacity = "1";
+  this.element.style.visibility = "visible";
+
+}
+/*
+Modal.prototype.close = function(){
+  this.containerModal.style.opacity = "0";
+  this.containerModal.style.visibility = "hidden";
+}*/
+
+export default Modal;
